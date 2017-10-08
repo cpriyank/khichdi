2d2710004f51935921fca3a94d7e7441e90661ea
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-10-08 14:26:28 -0700

    improved commenting

---
+++ csvwrapper/doc.go
@@ -0,0 +1,4 @@
+package csvwrapper
+
+// package csvwrapper is a wrapper around go's encoding/csv package to read and
+// write csv files
--- csvwrapper/to_2D_string_slice.go
+++ csvwrapper/to_2D_string_slice.go
@@ -1,5 +1,8 @@
 package csvwrapper
 
+// package csvwrapper is a wrapper around go's encoding/csv package to read and
+// write csv files
+
 import (
 	"bytes"
 	"encoding/csv"
@@ -7,10 +10,25 @@ import (
 	"io/ioutil"
 )
 
+// read a csv file specified by filepath as a slice of strings
+// e.g. If the file contains:
+// first_name,last_name,username
+// "Rob","Pike",rob
+// Ken,Thompson,ken
+// "Robert","Griesemer","gri"
+//
+// it returns:
+// [first_name last_name username]
+// [Rob Pike rob]
+// [Ken Thompson ken]
+// [Robert Griesemer gri]
+
 func CSVTo2DStringSlice(fileName string) ([][]string, error) {
 	in, err1 := ioutil.ReadFile(fileName)
 	buf := bytes.NewBuffer(in)
 	r := csv.NewReader(buf)
+	// encoding/csv allows specifying alternatives if fields are not separated by
+	// comma, like so:
 	// r.Comma = ';'
 	// r.Comment = '#'
 
