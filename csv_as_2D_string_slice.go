c5f5cec23cfe31af108f70807a9f9581deec085e
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-10-11 13:45:18 -0700

    Use library csv parser rather than writing from scratch

---
+++ csv_as_2D_string_slice.go
@@ -0,0 +1,19 @@
+package fileutils
+
+import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"io/ioutil"
+)
+
+func csvAs2DStringSlice(file string) ([][]string, error) {
+	in, err1 := ioutil.ReadFile(file)
+	buf := bytes.NewBuffer(in)
+	r := csv.NewReader(buf)
+	// r.Comma = ';'
+	// r.Comment = '#'
+
+	records, err2 := r.ReadAll()
+	return records, fmt.Errorf("%v : %v", err1, err2)
+}
